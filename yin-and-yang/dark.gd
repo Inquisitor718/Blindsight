1e151ba1ab09053658a609aa08bc192bb380b387
exharmonic <aryanbasra1138@gmail.com>
2026-02-21 00:49:49 +0530

    Added dash mechanism-2

--- yin-and-yang/dark.gd
+++ yin-and-yang/dark.gd
@@ -5,9 +5,14 @@ extends CharacterBody2D
 @export var jump_force : float = -400.0
 @export var gravity : float = 900.0
 @onready var hit_box: Area2D = $HitBox
-#@onready var collision_shape_2d: CollisionShape2D = $HitBox/CollisionShape2D
+@onready var dash_timer: Timer = $DashTimer
+@onready var dash_cooldown: Timer = $DashCooldown
 
 var is_attacking : bool = false
+
+var can_dash: bool = true
+var dash_speed = 900
+var is_dashing : bool = false
 
 @onready var sprite = $AnimatedSprite2D
 
@@ -16,24 +21,31 @@ var is_attacking : bool = false
 # -----------------------
 
 func _physics_process(delta):
+	var direction = 0
 
+	velocity.x = direction * dash_speed
+	
 	 # Apply gravity
 	if not is_on_floor():
 		velocity.y += gravity * delta
 
 	 # Movement input
-	var direction = 0
 
 	if Input.is_action_pressed("p1_left"):
-		direction -= 1
+		direction = -1
 	if Input.is_action_pressed("p1_right"):
-		direction += 1
+		direction = 1
+	if Input.is_action_just_pressed("dash") and not is_dashing and can_dash:
+		start_dash()
+		dash_cooldown.start()
+		can_dash = false
+		
 
 	 # Apply horizontal movement
-	if not is_attacking:
+	if not is_attacking and not is_dashing:	
 		velocity.x = direction * speed
 	else:
-		velocity.x = 0   # Stop while attacking
+		velocity.x = direction * dash_speed   # Stop while attacking
 
 	 # Flip sprite
 	if direction != 0:
@@ -47,9 +59,9 @@ func _physics_process(delta):
 	 # Attack
 	if Input.is_action_just_pressed("atk2") and not is_attacking:
 		attack()
+		
 
 	 # Move character
-	move_and_slide()
 
 	 # Animations
 	handle_animations(direction)
@@ -60,9 +72,10 @@ func _physics_process(delta):
 	else:
 		hit_box.monitoring = false
 		DarkGlobals.hit_box_monitoring = false
-	
-	#if DarkGlobals.hit_box_monitoring && hit_box.visible:
-		#print("Yes")
+		
+	move_and_slide()
+		#if DarkGlobals.hit_box_monitoring && hit_box.visible:
+			#print("Yes")
 
 # -----------------------
 # == ATTACK FUNCTION ==
@@ -74,8 +87,12 @@ func attack():
 	sprite.play("attack")
 	await get_tree().create_timer(0.4).timeout
 	is_attacking = false
-
-
+	
+func start_dash():
+	is_dashing = true
+	await get_tree().create_timer(0.1).timeout
+	is_dashing = false
+	
 # -----------------------
 # == ANIMATION HANDLER ==
 # -----------------------
@@ -102,3 +119,10 @@ func _on_AnimatedSprite2D_animation_finished():
 
 	if sprite.animation == "attack":
 		is_attacking = false
+
+
+func _on_dash_timer_timeout() -> void:
+	is_dashing = false
+ 
+func _on_dash_cooldown_timeout() -> void:
+	can_dash = true
